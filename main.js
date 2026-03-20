ef3bd309fa7324e404dc21de2c6f59bad0925f6c
ThePurpleFrogGamblingRebellion <c.mccaffrey2@share.epsb.ca>
2026-03-20 08:46:25 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 
+
+MPP.client.on("a", msg => {
+  if (msg.a == "+phobos") {
+    MPP.chat.send("Phobos commands: +start")
+  } else if (msg.a == "+start") {
+    MPP.chat.send("Hit note A-1")
+  }
+})
+
+MPP.client.on("n", note => {
+  if note.n.includes("a-1") {
+    MPP.chat.send("Nice")
+  }
+})
