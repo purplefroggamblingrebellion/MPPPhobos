b22010ee2566328043c520a87df12e6515d140ed
ThePurpleFrogGamblingRebellion <c.mccaffrey2@share.epsb.ca>
2026-03-20 13:31:50 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,18 +4,27 @@ const template = JSON.stringify(
 )
 const op = MPP.piano.play;
 let currentnote
+let currentplayid
+let currentplayglobal
 MPP.client.sendArray([
     {
         m: "userset",
         set: { name: "Phobos (+pb)" }
     }
 ]);
+function checkstor(id) {
+  if (localStorage.getItem(id) == null) {
+      localStorage.setItem(id,template)
+  }
+}
 
 MPP.client.on("a", msg => {
   if (msg.a == "+pb") {
     MPP.chat.send("Phobos commands: +start +score +slam")
   } else if (msg.a == "+start") {
     currentnote = Object.keys(MPP.piano.keys)[Math.floor(Math.random() * Object.keys(MPP.piano.keys).length)]
+    currentplayid = msg.p.id
+    currentplayglobal = msg.p._id
     MPP.chat.send("Hit note " + currentnote)
     hit = true
   } else if (msg.a == "+score") {
@@ -34,9 +43,17 @@ MPP.client.on("a", msg => {
 
 MPP.client.on("n", note => {
   note.n.forEach(n => {
-    if (n.n === currentnote && hit == true)  {
-      MPP.chat.send("Nice");
+    if (n.n === currentnote && hit == true && n.p == currentplayid)  {
+      MPP.chat.send("You hit the note! +5 points has been added. Use +score to see your points!");
+      checkstor(currentplayglobal)
+      let tempset = JSON.stringify(
+        JSON.parse(localStorage.getItem(currentplayglobal)).points + 5
+      )
+      localStorage.setItem(currentplayglobal,tempset)
       hit = false
+      currentplayid = ""
+      currentnote = ""
+      currentplayglobal = ""
     }
   });
 })
